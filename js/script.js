e794e463e1dcb9d1b33d6c3e00bc2c3ac0ad9eb4
Adroaldo <hygino@msn.com>
2022-10-08 15:44:38 -0300

    Listando dados estáticos no html

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ let tamanho = document.querySelector('#tamanho');
 let btnAdiciona = document.querySelector('#btnAdiciona');
 
 let btnAdicionaElemento = document.querySelector('#btnAdicionaElemento');
-let btnMostrarLista = document.querySelector('#btnMostraLista')
+let btnMostrarLista = document.querySelector('#btnMostraLista');
+let corpoTabela = document.querySelector('#corpo-tabela');
 
 let listaIntervalo = [];
 let listaElementos = [];
@@ -45,11 +46,76 @@ function adicionarElemento() {
 
 btnAdicionaElemento.addEventListener('click', adicionarElemento);
 
-function listarElementos() {
+/*function listarElementos() {
     listaElementos.map((x, index) => {
         console.log(`x_${index} = ${x}`);
     });
     console.log(`A lista tem ${listaElementos.length} elementos`);
 }
 
-btnMostrarLista.addEventListener('click', listarElementos);
+btnMostrarLista.addEventListener('click', listarElementos);*/
+
+
+let minhaLista = [];
+
+function adicionarLista(valor, x) {
+    for (let i = 0; i < x; i++) {
+        minhaLista.push(valor);
+    }
+}
+
+adicionarLista(141, 13);
+adicionarLista(146, 22);
+adicionarLista(151, 35);
+adicionarLista(156, 42);
+adicionarLista(161, 38);
+adicionarLista(166, 27);
+adicionarLista(171, 18);
+adicionarLista(176, 11);
+
+let min = 140;
+let max = 190;
+
+let intervalos = [];
+
+let a = 140;
+let b = 140;
+let intervalo = 5;
+
+while (b <= max) {
+    a = b;
+    b = a + intervalo;
+    //let media = (a + b) / 2;//acha a média do intervalo
+    let quantidade = minhaLista.filter(x => x >= a && x < b);//filtra os elementos que estejam no intervalo
+    if (quantidade.length !== 0) {
+        intervalos.push([a, b, quantidade.length]);
+    }
+}
+
+function gerarTabela() {
+    let freqAcumulada = 0;
+    let freqPercentualAcumulada = 0;
+    let total = minhaLista.length;
+    corpoTabela.innerHTML = '';
+    intervalos.map((x, index) => {
+        freqAcumulada += x[2];
+        let freqRelativa = freqAcumulada / total;
+        let freqRelativaPercentual = freqRelativa * 100.0;
+        freqPercentualAcumulada = freqAcumulada / 2;
+
+        corpoTabela.innerHTML +=
+            `
+            <tr>
+                <th scope="row">${index+1}</th>
+                <td>${x[0]} &#10152; ${x[1]}</td>
+                <td>${x[2]}</td>
+                <td>${freqAcumulada}</td>
+                <td>${freqRelativa.toFixed(3)}</td>
+                <td>${freqRelativaPercentual.toFixed(3)}</td>
+                <td>${freqPercentualAcumulada}</td>
+            </tr>
+            `;
+    });
+}
+
+btnMostrarLista.addEventListener('click', gerarTabela);
